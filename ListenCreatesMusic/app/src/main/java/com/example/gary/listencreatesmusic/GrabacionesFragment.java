a28752d765d79a79f94e253b457435a33efa50c5
Gary Calle <joevermelho@protonmail.com>
2018-09-16 23:11:36 -0500

    cardview grabacion y proyecto
    add fragments and floating action button foreach fragment

---
+++ ListenCreatesMusic/app/src/main/java/com/example/gary/listencreatesmusic/GrabacionesFragment.java
@@ -0,0 +1,33 @@
+package com.example.gary.listencreatesmusic;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class GrabacionesFragment extends Fragment {
+
+
+    public GrabacionesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        View rootView = inflater.inflate(R.layout.fragment_grabaciones, container, false);
+        return rootView;
+
+    }
+
+
+}
---
+++ ListenCreatesMusic/app/src/main/java/com/example/gary/listencreatesmusic/ProyectoFragment.java
@@ -0,0 +1,27 @@
+package com.example.gary.listencreatesmusic;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+
+public class ProyectoFragment extends Fragment {
+    public ProyectoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_proyecto, container, false);
+        return rootView;
+
+    }
+}
